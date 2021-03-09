91c635564cdf4e85fee66bc7947f4ae35f059f25
Brett Bejcek <brett@Bretts-Macbook.home>
2021-03-08 15:34:03 -1000

    fixed both blank screen and font size on rolls

--- components/Container.js
+++ components/Container.js
@@ -20,8 +20,18 @@ state = {
     marginValue: new Animated.Value(0)
   };
 
+
+fadeIn = new Animated.Value(0);
+
 async componentDidMount() {
+  Animated.timing(this.fadeIn, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: true,
+      delay:2000
+    }).start();
   const { status } = await Contacts.requestPermissionsAsync();
+
   if (status === 'granted') {
     this.props.loadContactsInitialize();
     this.props.loadKeysInitialize();
@@ -89,8 +99,15 @@ handleOnClick = () => {
 
 render(){
   const { homeScreen, phone, storageKeys, contacts, error, toggleScreen, name, marginLeft, onboarded } = this.props;
+  const fontSizeName = name.length > 15 ? RFPercentage(6.5)- (Math.min(name.length,30) * (RFPercentage(0.1))*(75/100)) : RFPercentage(6.5);
   if(onboarded==null){
-    return(<View/>)
+    return(
+      <View style={{flex:1,justifyContent: "center",alignItems: "center"}}>
+      <Animated.Text style={{ opacity: this.fadeIn, textAlign:`center`, padding:30, fontFamily:'BebasNeue_400Regular', fontSize:RFPercentage(4) }}>
+        Allow Out of the Blue to access your contacts in your phone settings and restart this app. Not to worry, no tracking analytics are used and no one will be able to see your contacts except for you.
+      </Animated.Text>
+
+</View>)
   }
   else if(onboarded==true){
       if(homeScreen){
@@ -100,15 +117,15 @@ render(){
         flexDirection: 'column',
         paddingTop:`10%`
       }}>
-        <TouchableOpacity onPress={this.handleOnClick}><Text style={{alignSelf: 'flex-end', right:30, fontFamily:'BebasNeue_400Regular', fontSize:RFPercentage(2.5), color:`gray`}}>-->Settings</Text></TouchableOpacity>
+        <TouchableOpacity onPress={this.handleOnClick}><Text style={{alignSelf: 'flex-end', right:30, fontFamily:'BebasNeue_400Regular', fontSize:RFPercentage(2.5), color:`gray`}}> > Settings</Text></TouchableOpacity>
         <View style={{width: `100%`, height: `40%`, float:`right`}}>
         <Animated.View style ={{marginLeft:this.state.marginValue}}><Image style={{height:`100%`, width:`100%`, overflow:`visible`}} source={home} /></Animated.View>
         </View>
         <View style={{width: `100%`, minHeight: `40%`}}>
         <View style ={{left: 30, marginTop:20, flex:1}}>
         <TouchableOpacity disabled = {!this.props.enabled} onPress={this.removeClicked}><Text style = {[{paddingTop:30,fontFamily:'BebasNeue_400Regular', fontSize:RFPercentage(2.25), textDecorationLine:`underline`}, this.props.enabled? {color:'black'} : {color:'lightgray'}]}>[REMOVE FROM FUTURE SHUFFLES]</Text></TouchableOpacity>
-        <TypeWriter style = {{width:`300%`,color:`gray`,minHeight: 100,fontSize:RFPercentage(6.5), paddingTop:30, fontFamily:'BebasNeue_400Regular'}} typing={1}><Text numberOfLines={1}>{name}</Text></TypeWriter>
-        <TypeWriter style = {{width:`300%`, color:`gray`,minHeight: 100,fontSize:RFPercentage(4), fontFamily:'BebasNeue_400Regular'}} typing={1}>{phone}</TypeWriter>
+        <View style={{justifyContent: 'center', minHeight: 100}}><TypeWriter numberOfLines={1} style = {{justifyContent:`center`,width:`100%`,color:`gray`,fontSize:fontSizeName, paddingTop:30, fontFamily:'BebasNeue_400Regular'}} typing={1}><Text numberOfLines={1}>{name}</Text></TypeWriter></View>
+        <TypeWriter style = {{width:`300%`, color:`gray`,minHeight: 100,fontSize:Math.min(RFPercentage(4), fontSizeName), fontFamily:'BebasNeue_400Regular'}} typing={1}>{phone}</TypeWriter>
         <View style = {{display:`flex`, flexDirection:`row`}}>
         <TouchableOpacity disabled = {!this.props.enabled} onPress={this.text}><Text style = {[{fontFamily:'BebasNeue_400Regular', fontSize:RFPercentage(3.5), textDecorationLine:`underline`}, this.props.enabled? {color:'black'} : {color:'lightgray'}]}>TEXT THEM</Text></TouchableOpacity>
         <TouchableOpacity disabled = {!this.props.enabled} onPress={this.call}><Text style = {[{fontFamily:'BebasNeue_400Regular', marginLeft:20, fontSize:RFPercentage(3.5), textDecorationLine:`underline`}, this.props.enabled? {color:'black'} : {color:'lightgray'}]}>CALL THEM</Text></TouchableOpacity>
@@ -126,7 +143,7 @@ render(){
         flexDirection: 'column',
         paddingTop:`10%`,
       }}>
-        <TouchableOpacity onPress={this.handleOnClick}><Text style={{alignSelf: 'flex-end', right:30, fontFamily:'BebasNeue_400Regular', fontSize:RFPercentage(2.5), color:`gray`}}>{'<--Home'}</Text></TouchableOpacity>
+        <TouchableOpacity onPress={this.handleOnClick}><Text style={{alignSelf: 'flex-end', right:30, fontFamily:'BebasNeue_400Regular', fontSize:RFPercentage(2.5), color:`gray`}}>{'< Home'}</Text></TouchableOpacity>
         <View style={{width: `100%`, height: `20%`,float:`right`,overflow:`visible`}}>
         <Image style={{height:`100%`, width:`120%`, marginLeft:-50, overflow:`visible`, resizeMode: 'contain'}} source={home} />
         </View>
